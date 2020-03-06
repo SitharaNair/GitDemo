2bb2de7afea9a301e35c542b8da8a5ff0c7fdcad
SitharaNair <sitharaponnath@gmail.com>
2020-03-06 20:15:02 +0530

    architect asian development

--- src/test/java/Seleniumacademy/APITest.java
+++ src/test/java/Seleniumacademy/APITest.java
@@ -8,6 +8,16 @@ public class APITest {
 	public void Post()
 	{
 		System.out.println("api test");
+		System.out.println("api test git 1");
+		System.out.println("api test git 2");
+		System.out.println("api test git 3");
+	}
+	public void Post2()
+	{
+		System.out.println("api test");
+		System.out.println("api test git 1");
+		System.out.println("api test git 2");
+		System.out.println("api test git 3");
 	}
 
 }
--- src/test/java/Seleniumacademy/SeleniumTest.java
+++ src/test/java/Seleniumacademy/SeleniumTest.java
@@ -14,4 +14,11 @@ public class SeleniumTest {
 		System.out.println("Gitx test 1");
 		System.out.println("Gitx test 2");
 	}
+	@Test
+	public void Seleniumtest2()
+	{
+		System.out.println("selenium test");
+		System.out.println("Git test 1");
+		System.out.println("Git test 2");
+	}
 }
