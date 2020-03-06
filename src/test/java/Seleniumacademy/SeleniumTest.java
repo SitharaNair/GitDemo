815199bdfdaaca1b13a7b89b5e0ddf78be602f9a
SitharaNair <sitharaponnath@gmail.com>
2020-03-06 19:46:51 +0530

    american guy coomits

--- src/test/java/Seleniumacademy/SeleniumTest.java
+++ src/test/java/Seleniumacademy/SeleniumTest.java
@@ -9,5 +9,9 @@ public class SeleniumTest {
 		System.out.println("selenium test");
 		System.out.println("Git test 1");
 		System.out.println("Git test 2");
+		System.out.println("Gitx test 1");
+		System.out.println("Gitx test 2");
+		System.out.println("Gitx test 1");
+		System.out.println("Gitx test 2");
 	}
 }
